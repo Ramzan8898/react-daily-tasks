f628d9a20af8470bc56b42a14b8a76dd840b6fd1
Ramzan <muhammadramzan3043@gmail.com>
2023-08-05 16:46:42 +0500

    input done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  
+  state = {
+    tasks:[],
+    item:''
+  }
+  handleInput = (e) => {
+    this.setState({
+      item:e.target.value,
+      [e.target.name ] : e.target.value
+    })
+    console.log(e.target.value);
+  }
+  
+  render() {
+    return  <>
+    <h1 className='text-center mt-5 text-white'>Daily Task List</h1>
+    
+    <div className="container my-5">
+    <input type="text" placeholder='Your Task' className='form-control w-50 p-3 '
+    name='task' value={this.state.item} onChange={this.handleInput}/> 
     </div>
-  );
+    
+    
+    </>
+    
+    
+  }
 }
+
+
+
 
 export default App;
