4306c849d955ac7becb7eb53af38ec72c30fc116
Ramzan <muhammadramzan3043@gmail.com>
2024-05-25 10:11:21 +0500

    changes in App component

--- src/App.js
+++ src/App.js
@@ -20,19 +20,13 @@ class App extends Component {
     return  <>
     <h1 className='text-center mt-5 text-white'>Daily Task List</h1>
     
-    <div className="container my-5">
+    <div className="container my-5 d-flex justify-content-evenly">
     <input type="text" placeholder='Your Task' className='form-control w-50 p-3 '
     name='task' value={this.state.item} onChange={this.handleInput}/> 
+    <button className='btn btn-warning ' onClick={this.handleTask}>Add</button>
     </div>
     
-    
-    </>
-    
-    
+    </>   
   }
 }
-
-
-
-
 export default App;
