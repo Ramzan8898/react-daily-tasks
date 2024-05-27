c44648ed638903b9c3e83db4274ea7e2bf5d3b7e
Ramzan8898 <muhammadramzan3043@gmail.com>
2024-05-27 16:27:54 +0500

    editable content functionality working,try to save the edited content to local storage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { IoIosAddCircle } from "react-icons/io";
+import { GrEdit } from "react-icons/gr";
 import './App.css';
 
 class App extends React.Component {
@@ -8,10 +9,35 @@ class App extends React.Component {
     super()
     this.state = {
       tasks: [],
-      input: ''
+      input: '',
+      editIndex: null,
+      editInput: '',
+      hoveredRowIndex: null,
+      isContentEditable: false
     }
   }
 
+  handleMouseEnter = (index) => {
+    this.setState({ hoveredRowIndex: index });
+  }
+
+  handleEdit = (index) => {
+    let data = this.state.tasks[index];
+    this.setState({ editIndex: index, editInput: data });
+
+  }
+  handleEditChange = (event) => {
+    this.setState({ editInput: event.target.value });
+  }
+  saveEdit = (index) => {
+    const { tasks, editInput } = this.state;
+    tasks[index] = editInput;
+    this.setState({ tasks, editIndex: null, editInput: '' });
+  }
+
+  handleMouseLeave = () => {
+    this.setState({ hoveredRowIndex: null });
+  }
 
   componentDidMount() {
     const savedTodos = localStorage.getItem('tasks');
@@ -48,27 +74,49 @@ class App extends React.Component {
           name='task' value={this.state.input} onChange={(e) => this.setState({ input: e.target.value })} />
         <IoIosAddCircle onClick={this.addTodo} style={{ fontSize: '45px', color: 'white', cursor: 'pointer', marginLeft: '20px' }} />
       </div>
-      <div className='container' >
-        <table className='table table-dark table-hover border-primary w-25 rounded-pil'>
-          <thead>
-            <tr>
-              <th className='h4'>#</th>
-              <th scope="col">
-                <h5 className=' text-white'>Tasks</h5>
-              </th>
-            </tr>
-          </thead>
+      <div className='container'>
+        <table className='table table-hovered w-25 table-dark'>
           {this.state.tasks.length !== 0 ? (
             <tbody className='w-100'>
               {this.state.tasks.map((task, index) => (
-                <tr key={index}>
+                <tr
+                  key={index}
+                  onMouseEnter={() => this.handleMouseEnter(index)}
+                  onMouseLeave={this.handleMouseLeave}
+                >
                   <td>{index + 1}</td>
-                  <td>{task}</td>
+                  <td>
+                    {this.state.editIndex === index ? (
+                      <input
+                        type="text"
+                        value={this.state.editInput}
+                        onChange={this.handleEditChange}
+                        onBlur={() => this.saveEdit(index)}
+                        onKeyPress={(event) => {
+                          if (event.key === 'Enter') {
+                            this.saveEdit(index);
+                          }
+                        }}
+                      />
+                    ) : (
+                      task
+                    )}
+                  </td>
+                  <td>
+                    {this.state.hoveredRowIndex === index && (
+                      <GrEdit onClick={() => this.handleEdit(index)} />
+                    )}
+                  </td>
                 </tr>
               ))}
             </tbody>
-          ) : null}
-
+          ) : (
+            <tbody>
+              <tr>
+                <td colSpan="3">No todos found.</td>
+              </tr>
+            </tbody>
+          )}
         </table>
 
       </div>
